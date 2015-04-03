9310f8573b577b67b7afacede905fd9cecc74d7a
Viet Nguyen <nguyenquocviet@tma.com.vn>
2015-04-03 17:22:33 +0700

    first commit

---
+++ app/view1/view1.js
@@ -0,0 +1,59 @@
+'use strict';
+
+angular.module('myApp.view1', ['ngRoute'])
+
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+            .when('/view1', {
+                templateUrl: 'view1/view1.html',
+                controller: 'View1Ctrl'
+            })
+            .when('/details/:id', {
+                templateUrl: 'view1/details.html',
+                controller: 'DetailCtrl'
+            });
+    }])
+
+
+
+    .controller('View1Ctrl', ['$scope', function ($scope) {
+        $scope.products = [
+            {
+                id: 1,
+                title: "product1",
+                imgUrl: "view1/img.jpg",
+                price: "20$",
+                detailPage: "detailURl"
+            },
+            {
+                id: 2,
+                title: "product2",
+                imgUrl: "view1/img.jpg",
+                price: "20$",
+                detailPage: "detailURl"
+            },
+            {
+                id: 3,
+                title: "product3",
+                imgUrl: "view1/img.jpg",
+                price: "20$",
+                detailPage: "detailURl"
+            },
+            {
+                id: 4,
+                title: "product1",
+                imgUrl: "view1/img.jpg",
+                price: "20$",
+                detailPage: "detailURl"
+            },
+        ]
+        $scope.sayHello = function () {
+            console.log("Hello Everyone!");
+        }
+
+    }])
+
+
+    .controller('DetailCtrl', ['$scope', '$routeParams', function ($scope, $routeParams) {
+        $scope.id = $routeParams.id;
+    }])
