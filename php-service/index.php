4c892618a8a66837acb4e16ab0df07ec7779f1ad
Viet Nguyen <nguyenquocviet@tma.com.vn>
2015-04-06 17:40:47 +0700

    completed CRUD examples with SLIM

--- php-service/index.php
+++ php-service/index.php
@@ -3,13 +3,18 @@ require 'Slim/Slim.php';
 
 \Slim\Slim::registerAutoloader();
 
-$app = new \Slim\Slim(); 
+$app = new \Slim\Slim();
 
-$app->get('/hello', 'getHello');
+$app->get('/friends', 'getFriends');
+$app->get('/friend/:id',  'getFriend');
+$app->post('/friends', 'addFriend');
+$app->put('/friend/:id', 'updateFriend');
+$app->delete('/friend/:id', 'deleteFriend');
+$app->get('/friend/search/:query', 'findByName');
 
 $app->run();
 
-function getHello() {
+function getFriends() {
     $sql = "SELECT id, name, job FROM friends";
     try {
         $db = getConnection();
@@ -17,6 +22,87 @@ function getHello() {
         $hello = $stmt->fetchAll(PDO::FETCH_OBJ);
         $db = null;
         echo '{"hello": ' . json_encode($hello) . '}';
+    } catch(PDOException $e) {
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }
+}
+
+function getFriend($id) {
+    $sql = "SELECT * FROM friends WHERE id=:id";
+    try {
+        $db = getConnection();
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam("id", $id);
+        $stmt->execute();
+        $hello = $stmt->fetchObject();
+        $db = null;
+        echo json_encode($hello);
+    } catch(PDOException $e) {
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }
+}
+
+function addFriend() {
+    $request = \Slim\Slim::getInstance()->request();
+    $friend = json_decode($request->getBody());
+    $sql = "INSERT INTO friends (name, job) VALUES (:name, :job)";
+    try {
+        $db = getConnection();
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam("name", $friend->name);
+        $stmt->bindParam("job", $friend->job);
+        $stmt->execute();
+        $friend->id = $db->lastInsertId();
+        $db = null;
+        echo json_encode($friend);
+    } catch(PDOException $e) {
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }
+}
+
+function updateFriend($id) {
+    $request = \Slim\Slim::getInstance()->request();
+    $body = $request->getBody();
+    $friend = json_decode($body);
+    $sql = "UPDATE friends SET name=:name, job=:job WHERE id=:id";
+    try {
+        $db = getConnection();
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam("name", $friend->name);
+        $stmt->bindParam("job", $friend->job);
+        $stmt->bindParam("id", $id);
+        $stmt->execute();
+        $db = null;
+        echo json_encode($friend);
+    } catch(PDOException $e) {
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }
+}
+
+function deleteFriend($id) {
+    $sql = "DELETE FROM friends WHERE id=:id";
+    try {
+        $db = getConnection();
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam("id", $id);
+        $stmt->execute();
+        $db = null;
+    } catch(PDOException $e) {
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }
+}
+
+function findByName($query) {
+    $sql = "SELECT * FROM friends WHERE UPPER(name) LIKE :query ORDER BY name";
+    try {
+        $db = getConnection();
+        $stmt = $db->prepare($sql);
+        $query = "%".$query."%";
+        $stmt->bindParam("query", $query);
+        $stmt->execute();
+        $friends = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $db = null;
+        echo '{"friends": ' . json_encode($friends) . '}';
     } catch(PDOException $e) {
         echo '{"error":{"text":'. $e->getMessage() .'}}';
     }
